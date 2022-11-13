f9fb5b306dfe7197f2ca0950be31d575f6706f39
Halisson.Duraes <Halisson.Duraes@sicoob.com.br>
2022-11-13 11:38:01 -0300

    logging

--- src/email/account.js
+++ src/email/account.js
@@ -1,10 +1,12 @@
 const sendgridAPIKey = process.env.SENDGRID_API_KEY;
 
 const sendWelcomeEmail = (email, name) => {
+    console.log('sendgrid', sendgridAPIKey);
     console.log('Email sent to:', email, ':', name);
 }
 
 const sendCancelationEmail = (email, name) => {
+    console.log('sendgrid', sendgridAPIKey);
     console.log('Email sent to:', email, ':', name);
 }
 
